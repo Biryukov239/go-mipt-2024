2961e435ade018a8da86129826f7267208193815
gregory <gregory239@mail.ru>
2024-10-12 20:45:53 +0300

    afn '

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,7 +2,10 @@
 
 package hotelbusiness
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 type Guest struct {
 	CheckInDate  int
@@ -15,37 +18,49 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	result := make([]Load, 0)
+	var result []Load
 	if len(guests) == 0 {
 		return result
 	}
-	mapResult := make(map[int]int)
 	sort.Slice(guests, func(i, j int) bool {
 		return guests[i].CheckInDate < guests[j].CheckInDate
 	})
-	for _, elem := range guests {
-		mapResult[elem.CheckInDate] = 0
-		mapResult[elem.CheckOutDate] = 0
+	guestCount := 0
+	result = append(result, Load{guests[0].CheckInDate, guestCount + 1})
+	guestCount++
+	if len(guests) == 1 {
+		result = append(result, Load{guests[0].CheckOutDate, guestCount - 1})
+		guestCount--
+		return result
 	}
-	mapResult[guests[0].CheckInDate]++
-	for i := range guests[1:] {
+	for i := 1; i < len(guests); i++ {
+		if guests[i-1].CheckInDate == guests[i].CheckInDate {
+			result[i-1].GuestCount++
+			fmt.Print("We're here!")
+		}
+		if guests[i].CheckInDate > guests[i-1].CheckOutDate {
+			result = append(result, Load{guests[i-1].CheckOutDate, guestCount - 1})
+			guestCount--
+			result = append(result, Load{guests[i].CheckInDate, guestCount + 1})
+			guestCount++
+			result = append(result, Load{guests[i].CheckOutDate, guestCount - 1})
+			guestCount--
+		}
 		if guests[i-1].CheckOutDate == guests[i].CheckInDate {
-			continue
+			result = append(result, Load{guests[i].CheckOutDate, guestCount - 1})
+			guestCount--
 		}
-		if guests[i-1].CheckOutDate < guests[i].CheckInDate {
-			mapResult[guests[i].CheckInDate]++
-			continue
+		if guests[i].CheckInDate < guests[i-1].CheckOutDate && guests[i-1].CheckInDate != guests[i].CheckInDate {
+			result = append(result, Load{guests[i].CheckInDate, guestCount + 1})
+			guestCount++
+			result = append(result, Load{guests[i-1].CheckOutDate, guestCount - 1})
+			guestCount--
+			result = append(result, Load{guests[i].CheckOutDate, guestCount - 1})
+			guestCount--
 		}
-		if guests[i].CheckOutDate > guests[i-1].CheckOutDate {
-			mapResult[guests[i].CheckInDate] = mapResult[guests[i-1].CheckInDate] + 1
-			mapResult[guests[i-1].CheckOutDate] = mapResult[guests[i].CheckInDate] - 1
-			continue
+		if guests[i-1].CheckOutDate == guests[i].CheckOutDate {
+			result = append(result, Load{guests[i].CheckOutDate, 0})
 		}
-		mapResult[guests[i].CheckInDate] = mapResult[guests[i-1].CheckInDate] + 1
-		mapResult[guests[i].CheckOutDate] = mapResult[guests[i].CheckInDate] - 1
-	}
-	for key, value := range mapResult {
-		result = append(result, Load{key, value})
 	}
 	return result
 }
